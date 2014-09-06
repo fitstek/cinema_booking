ce0c001b336cbbd77ba36ccfebcc2b1da36432f5
fitstek <fitsum.teklehaimanot@gmail.com>
2014-09-06 19:28:15 +0100

    row contains 50 unbooked seats upon initialization

--- lib/row.rb
+++ lib/row.rb
@@ -1,12 +1,18 @@
 class Row
 
 	def initialize(row_number)
+		@seats ||= []
 		raise "Please pick a seat number between 0 and 99" if inexistent?(row_number)
 		@number = row_number
+		0.upto(49).each do | seat_number|
+			@seats << Seat.new(row_number, seat_number)
+		end
+		@seats
 	end
 
 	attr_reader :number
-
+	attr_reader :seats
+	
 	def inexistent?(row_number)
 		row_number < 0 || row_number > 99
 	end

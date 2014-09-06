a91fd8bf64474e6ae98b14d74e9b5a41ff5fc184
fitstek <fitsum.teklehaimanot@gmail.com>
2014-09-06 18:25:51 +0100

    seat is initialized with seat number and row number

--- lib/seat.rb
+++ lib/seat.rb
@@ -1,7 +1,12 @@
 class Seat
-	def initialize
+	def initialize(row_number, seat_number)
 		@booked = false
+		@row = row_number
+		@number = seat_number
 	end
+
+	attr_reader :row
+	attr_reader :number
 
 	def booked?
 		@booked
--- spec/seat_spec.rb
+++ spec/seat_spec.rb
@@ -1,8 +1,19 @@
 require 'seat'
 
 describe Seat do
-	let(:seat) { Seat.new }
+	# let(:seat) { Seat.new }
 	it 'is available initially' do
+		seat = Seat.new(0,0)
 		expect(seat).not_to be_booked
+	end
+	context 'seat and row numbers' do
+		it 'knows its seat number when created' do
+			seat = Seat.new(0,0)
+			expect(seat.number).to eq 0
+		end
+		it 'knows its row number when created' do
+			seat = Seat.new(0,0)
+			expect(seat.row).to eq 0
+		end
 	end
 end

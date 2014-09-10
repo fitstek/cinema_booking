a38965ef4ff4a68dec039fb05832ce6a564a4934
fitstek <fitsum.teklehaimanot@gmail.com>
2014-09-10 05:17:19 +0100

    booking is initialized

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,11 +1,15 @@
 class Booking
 	@@ids = 0
 	
-	def initialize
+	def initialize(first_seat_row, first_seat_number, last_seat_row,last_seat_number)
 		@@ids += 1
 		@id = @@ids
+		@first_seat = Seat.new(first_seat_row, first_seat_number)
+		@last_seat = Seat.new(last_seat_row, last_seat_number)
 	end
 
 	attr_reader :id
+	attr_reader :first_seat
+	attr_reader :last_seat
 
 end
--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -1,8 +1,8 @@
 require 'booking'
 
 describe Booking do
-	let(:booking) { Booking.new }
-	let(:booking2) { Booking.new }
+	let(:booking) { Booking.new(0,1,0,1) }
+	let(:booking2) { Booking.new(0,1,0,3) }
 	context 'initializes with booking ID, row, first seat and last seat' do
 		it 'has booking ID' do
 			expect(booking.id).to eq 1
@@ -10,6 +10,14 @@ describe Booking do
 
 		it 'booking ID automatically increments' do
 			expect(booking2.id).to eq 2
+		end
+
+		it 'takes the first and last positions of bookings(row,seat)' do
+			expect(booking.first_seat.number).to eq 1
+		end
+
+		it 'takes the first and last positions of bookings(row,seat)' do
+			expect(booking2.last_seat.number).to eq 3
 		end
 	end
 end

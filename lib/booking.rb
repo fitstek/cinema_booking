6a2e22fb66525c7c2f126e49ed6c192144749d84
fitstek <fitsum.teklehaimanot@gmail.com>
2014-09-12 13:34:51 +0100

    fixed all failing tests

--- lib/booking.rb
+++ lib/booking.rb
@@ -3,14 +3,14 @@ class Booking
 	
 	def initialize(first_seat_row, first_seat_number, last_seat_row,last_seat_number)
 		@@ids += 1
-		@seats ||= []
+		@seat_numbers ||= []
 		@id = @@ids
 		@first_seat = first_seat_number
 		@last_seat = last_seat_number
 		@first_seat_row = first_seat_row
 		@last_seat_row = last_seat_row
 		first_seat_number.upto(last_seat_number).each do |seat_number|
-			@seats << seat_number
+			@seat_numbers << seat_number
 		end
 	end
 
@@ -18,7 +18,7 @@ class Booking
 	attr_reader :id
 	attr_reader :first_seat
 	attr_reader :last_seat
-	attr_reader :seats
+	attr_reader :seat_numbers
 	attr_reader :last_seat_row
 	attr_reader :first_seat_row
 
--- lib/booking_checks.rb
+++ lib/booking_checks.rb
@@ -5,31 +5,31 @@ module BookingChecks
 	end
 
 	def less_than_six_seats?(booking_request)
-		booking_request.seats.count < 6
+		booking_request.seat_numbers.count < 6
 	end
 
-	def seats_available?(booking_request)
-		# booking_request.seats.all? { |seat| seat.available? }
+	def seats_available?(booking_request, hall)
+		booking_request.seat_numbers.all? { |seat_number| hall.rows[booking_request.first_seat_row].seats[seat_number].available? }
 	end
 
-	# def leaves_more_than_one_gap?(booking_request)
-	# 	first_seat_row = booking_request.first_seat.row
-	# 	last_seat_row = booking_request.last_seat.row
-	# 	first_seat_number = booking_request.first_seat.number 
-	# 	last_seat_number = booking_request.last_seat.number 
+	def leaves_more_than_one_gap?(booking_request, hall)
+		first_seat_row = booking_request.first_seat_row
+		last_seat_row = booking_request.last_seat_row
+		first_seat_number = booking_request.first_seat
+		last_seat_number = booking_request.last_seat
 
-	# 	if booking_request.hall.rows[first_seat_row].seats.all? {|seat| seat.available?}
-	# 		first_seat_number != 1 && last_seat_number != 48
-	# 	elsif first_seat_number != 1 && booking_request.hall.rows[first_seat_row].seats[(first_seat_number - 2)].booked?
-	# 		booking_request.hall.rows[first_seat_row].seats[(first_seat_number - 1)].available? ? false : true
-	# 	elsif last_seat_number != 48 && booking_request.hall.rows[last_seat_row].seats[(last_seat_number + 2)].booked? 
-	# 		booking_request.hall.rows[last_seat_row].seats[(last_seat_number + 1)].available? ? false : true
-	# 	elsif first_seat_number == 1
-	# 		booking_request.hall.rows[first_seat_row].seats[0].available? ? false : true
-	# 	elsif last_seat_number == 48
-	# 		booking_request.hall.rows[last_seat_row].seats[49].available? ? false : true
-	# 	else
-	# 		true
-	# 	end
-	# end
+		if hall.rows[first_seat_row].seats.all? {|seat| seat.available?}
+			first_seat_number != 1 && last_seat_number != 48
+		elsif first_seat_number != 1 && hall.rows[first_seat_row].seats[(first_seat_number - 2)].booked?
+			hall.rows[first_seat_row].seats[(first_seat_number - 1)].available? ? false : true
+		elsif last_seat_number != 48 && hall.rows[last_seat_row].seats[(last_seat_number + 2)].booked? 
+			hall.rows[last_seat_row].seats[(last_seat_number + 1)].available? ? false : true
+		elsif first_seat_number == 1
+			hall.rows[first_seat_row].seats[0].available? ? false : true
+		elsif last_seat_number == 48
+			hall.rows[last_seat_row].seats[49].available? ? false : true
+		else
+			true
+		end
+	end
 end

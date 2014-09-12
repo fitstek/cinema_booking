1d8ae12f71427eb4bc73800a0e4c6bc574098ceb
fitstek <fitsum.teklehaimanot@gmail.com>
2014-09-12 14:30:29 +0100

    checking before booking added, not working as expected, more work required

--- lib/cinema_hall.rb
+++ lib/cinema_hall.rb
@@ -15,9 +15,15 @@ class CinemaHall
 	attr_reader :number
 	attr_reader :rows
 
-	def book(booking_request)
-		booking_request.seat_numbers.each do |seat_number|
-			rows[booking_request.first_seat_row].seats[seat_number].available = false
+	def book(booking_request, hall)
+		if valid?(booking_request, hall)
+			booking_request.seat_numbers.each do |seat_number|
+				rows[booking_request.first_seat_row].seats[seat_number].available = false
+			end
 		end
+	end
+
+	def valid?(booking_request, hall)
+		seats_on_the_same_row?(booking_request) && less_than_six_seats?(booking_request) && seats_available?(booking_request, hall) && leaves_more_than_one_gap?(booking_request, hall)
 	end
 end
--- spec/cinema_hall_spec.rb
+++ spec/cinema_hall_spec.rb
@@ -3,6 +3,7 @@ require 'cinema_hall'
 describe CinemaHall do
 	let(:hall) { CinemaHall.new(1) }
 	let(:booking) { Booking.new(0,1,0,1) }
+	let(:booking2) { Booking.new(0,1,0,4) }
 	it 'initializes with a hall number' do
 		expect(hall.number).to eq 1
 	end
@@ -12,8 +13,14 @@ describe CinemaHall do
 	end
 
 	it 'books seats' do
-		hall.book(booking)
+		hall.book(booking, hall)
 		expect(hall.rows[0].seats[1]).to be_booked
+	end
+
+	it 'books seats that meet the criterion' do
+		hall.book(booking, hall)
+		hall.book(booking2, hall)
+		expect(hall.rows[0].seats[2]).not_to be_booked
 	end
 
 end

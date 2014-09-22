31e9b22ef425fb703bd91dc8802d7a43dbeefebb
fitstek <fitsum.teklehaimanot@gmail.com>
2014-09-22 05:58:46 +0100

    made changes in app.rb to reflect previous changes to the program

--- app.rb
+++ app.rb
@@ -4,27 +4,42 @@ require_relative 'lib/cinema_hall'
 require_relative 'lib/row'
 require_relative 'lib/seat'
 
-
-@hall = CinemaHall.new(1)
+@hall = CinemaHall.new(hall_number: 1)
 
 def hall
-  @hall 
+  @hall
 end
 
-def input_from
-  puts "Please put the path of the file to be processed"
-  file_path = gets.chomp
-  File.open(file_path, "r").each_line do | booking_request|
-    params = booking_request.split(/\D/).drop(1)
-    first_row = params[1].to_i
-    first_seat = params[2].to_i
-    last_row = params[3].to_i
-    last_seat = params[4].to_i
-    booking = Booking.new(first_row, first_seat, last_row, last_seat)
+def user_input
+  puts 'Please put the path of the file to be processed'
+  gets.chomp
+end
+
+def output_message
+  puts "Total number of bookings rejected: #{hall.rejected.count}"
+  puts 'IF you want see the rejected bookings type: hall.rejected'
+end
+
+def book_from(user_input)
+  read_file_from(user_input).each_line do | booking_request|
+    booking = Booking.new(get_hash_from(booking_request))
     hall.book(booking, hall)
   end
-    puts "Total number of bookings rejected: #{@hall.rejected.count}"
-    puts "IF you want see the rejected bookings type: hall.rejected"
 end
 
-input_from
+def read_file_from(file_path)
+  File.open(file_path, 'r')
+end
+
+def get_hash_from(booking_request)
+  params = booking_request.split(/\D/).drop(1)
+  hash = {}
+  hash[:first_row] = params[1].to_i
+  hash[:first_seat] = params[2].to_i
+  hash[:last_row] = params[3].to_i
+  hash[:last_seat] = params[4].to_i
+  hash
+end
+
+book_from(user_input)
+output_message
